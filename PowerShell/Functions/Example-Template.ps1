bca95be303c96b15b0164e1a9a9e645230842171
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2019-04-25 00:21:06 -0400

    Example Template

---
+++ PowerShell/Functions/Example-Template.ps1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.LINK
+
+.NOTES
+          FileName: Untitled-1
+          Author: Jordan Benzing
+          Contact: @JordanTheItGuy
+          Created: 2019-04-25
+          Modified: 2019-04-25
+
+          Version - 0.0.0 - (2019-04-25)
+
+
+          TODO:
+               [ ] Script Main Goal
+               [ ] Script Secondary Goal
+
+.Example
+
+#>
+
+[cmdletbinding()]
+param()
+begin{}
+process{}
