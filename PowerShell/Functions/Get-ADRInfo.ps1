bc74259c043707623cbfec8a2d2402b9f412db87
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2019-04-30 09:12:37 -0400

    Create Get-ADRInfo.ps1

---
+++ PowerShell/Functions/Get-ADRInfo.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+.LINK
+
+
+.NOTES
+          FileName: Get-ADRInfo
+          Author: Jordan Benzing
+          Contact: @JordanTheItGuy
+          Created: 2019-04-29
+          Modified: 2019-04-29
+
+          Version - 0.0.0 - (2019-04-29)
+
+
+          TODO:
+               [ ] Function to retrieve Configuration Manager ADRS
+               
+
+.Example
+
+#>
+
+[cmdletbinding()]
+param(
+
+)
+begin
+{
+
+}
+process
+{
+
+}
