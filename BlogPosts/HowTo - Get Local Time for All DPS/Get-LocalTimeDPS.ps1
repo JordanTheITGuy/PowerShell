eeef8a21fa62932682fcdb6c2272735fb8113ebe
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2019-08-26 15:42:13 -0400

    Removed the filter for content on the C:\

--- BlogPosts/HowTo - Get Local Time for All DPS/Get-LocalTimeDPS.ps1
+++ BlogPosts/HowTo - Get Local Time for All DPS/Get-LocalTimeDPS.ps1
@@ -3,7 +3,7 @@ Import-Module (Join-Path $(Split-Path $ENV:SMS_ADMIN_UI_PATH) ConfigurationManag
 $Drive = (Get-PSDrive -PSProvider CMSite).Name
 Set-Location $($Drive + ":")
 $List = [System.Collections.Generic.List[object]]::new()
-$Servers = Get-CMDistributionPointInfo | ForEach-Object{if($_.NalPath -in $(Get-CMDistributionPointDriveInfo | Where-Object {$_.Drive -eq "C"}).NalPath){if(!(test-path -Path "\\$($_.ServerName)C$\NO_SMS_ON_DRIVE.SMS")){$_.ServerName}}}
+$Servers = Get-CMDistributionPointInfo | Select-Object -ExpandProperty ServerName
 ForEach($Server in $Servers){
     $TimeZone = Invoke-Command -ComputerName $Server -ScriptBlock {Get-TimeZone}
     $CurrentTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), "$($TimeZone.ID)")
@@ -14,6 +14,7 @@ ForEach($Server in $Servers){
     }
     $Item = New-Object -TypeName PSobject -Property $Hash
     $List.Add($Item)
+    return $List
 }
 
 #Function for getting the current time of all Distribution points.
