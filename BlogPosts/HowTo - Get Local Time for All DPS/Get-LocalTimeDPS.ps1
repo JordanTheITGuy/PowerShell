c266cca8f64012ea3c7ac9f86e50d25f67319a40
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2019-08-26 15:38:37 -0400

    Commit Get-LocalTime DP Changes and additions

--- BlogPosts/HowTo - Get Local Time for All DPS/Get-LocalTimeDPS.ps1
+++ BlogPosts/HowTo - Get Local Time for All DPS/Get-LocalTimeDPS.ps1
@@ -1,3 +1,4 @@
+#Option 1 - Assumes the ConfigMgrModule is available for use
 Import-Module (Join-Path $(Split-Path $ENV:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1) -Verbose:$false
 $Drive = (Get-PSDrive -PSProvider CMSite).Name
 Set-Location $($Drive + ":")
@@ -13,4 +14,44 @@ ForEach($Server in $Servers){
     }
     $Item = New-Object -TypeName PSobject -Property $Hash
     $List.Add($Item)
+}
+
+#Function for getting the current time of all Distribution points.
+function Get-DPCurrentTime{
+    [CmdletBinding()]
+    param(
+    [Parameter(HelpMessage = "Enter the name of the ConfigMgr Server",Mandatory = $true)]
+    [string]$ConfigMgrServer,
+    [Parameter(HelpMessage = "Enter the ConfigMgr Site Server", Mandatory = $true)]
+    [string]$SiteCode
+    )
+    begin{}
+    process{
+        $DPList = Get-WmiObject -ComputerName $ConfigMgrServer -Namespace root\sms\site_$SiteCode -Query "select distinct ServerName from sms_distributionpointInfo"
+        $List = [System.Collections.Generic.List[object]]::new()
+        ForEach($Server in $DPList){
+            $TimeZone = Invoke-Command -ComputerName $Server.ServerName -ScriptBlock {Get-TimeZone}
+            $CurrentTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), "$($TimeZone.ID)")
+            $Hash = [ordered]@{
+                ServerName = $Server.SERVERNAME
+                CurrentTime = $Currenttime
+                TimeZone = $TimeZone.DisplayName
+            }
+            $Item = New-Object -TypeName PSobject -Property $Hash
+            $List.Add($Item)
+        }
+    return $List
+    }
+}
+
+#Function for Getting the current time of a specific server
+function Get-RemoteTime {
+    [CmdletBinding()]
+    param(
+        [Parameter(HelpMessage = "The name of the remote computer")]
+        [string]$ComputerName
+    )
+    $TimeZone = Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-TimeZone}
+    $CurrentTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId((Get-Date), "$($TimeZone.ID)")
+    return $CurrentTime
 }
